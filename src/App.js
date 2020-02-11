30536372877e89a02da54486df314fc37cae1d38
Andrew Schenck <omnova@gmail.com>
2020-02-10 22:49:41 -0600

    Configure AWS

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './theme/Theme';
-// import Amplify from 'aws-amplify';
-// import awsconfig from './aws-exports';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
 import MainLayout from './MainLayout';
 
-function App() {
+Amplify.configure(awsconfig);
+
+function App() { 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />    
