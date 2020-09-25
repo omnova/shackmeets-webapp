2595bd6d8c51fb0d395b586f554a70a0992174f8
Andrew Schenck <omnova@gmail.com>
2020-09-25 14:09:05 -0500

    Redid amplify stuff to fix upgrade issues

--- amplify/backend/function/ShackmeetsAuthUserMigrationFunction/src/index.js
+++ amplify/backend/function/ShackmeetsAuthUserMigrationFunction/src/index.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 
 exports.handler = async (event, context, callback) => {
   const result = await tryMigrateUser(event, context);
@@ -27,6 +27,7 @@ async function tryMigrateUser(event, context) {
     }      
     else {
       // Return error to Amazon Cognito
+      console.log("Failed password check");
       return "Bad password";
     }
   }
@@ -39,21 +40,43 @@ async function checkShackCredentials(username, password) {
   const uri = 'https://winchatty.com/v2/verifyCredentials';
   const data = "username=" + encodeURIComponent(username) + '&password=' + encodeURIComponent(password);
 
+  console.log(data);
+
   try {
-    const response = await axios({
-      method: 'post',
-      url: uri,
+    // const response = await axios({
+    //   method: 'post',
+    //   url: uri,
+    //   headers: {
+    //     'Accept-Encoding': 'gzip, deflate, br',
+    //     'Content-Type': 'application/x-www-form-urlencoded'
+    //   },
+    //   data: data
+    // });
+
+    const response = await fetch(uri, {
+      method: 'POST',
       headers: {
         'Accept-Encoding': 'gzip, deflate, br',
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      data: data
+      body: data
     });
-    
-    
-    return response.isValid;
+  
+    const result = await response.json();
+    console.log(result);
+
+    if (result.isValid) {
+      console.log("shack credentials valid.");
+      return true;
+    }
+    else {
+      console.log("shack credentials invalid.");
+      return false;
+    }
   }     
   catch (ex) {
+    console.log("Exception during shack credential check.")
+    console.log(ex);
     return false;
   }
 }
--- src/contexts/auth/AuthProvider.js
+++ src/contexts/auth/AuthProvider.js
@@ -26,72 +26,14 @@ function AuthProvider({ children }) {
 
 
   const login = useCallback(async (username, password) => {
-    // await Auth.signIn({
-    //   username, // Required, the username
-    //   password // Optional, the password
-    // }).then(user => console.log(user))
-    //   .catch(err => console.log(err));
-
+  
     try {
-      Auth.configure({ authenticationFlowType: "USER_PASSWORD_AUTH"});
-      // Auth.configure({ authenticationFlowType: "CUSTOM_AUTH"});
-
       const user = await Auth.signIn(username, password);
 
       setUser(user);
       return user;
     } catch (error) {
-        console.log('error signing in', error);
-    }
-
-    // await Auth.signIn(username, password)
-    //   .then(user => {
-    //     console.log(user);
-    //     if (user.challengeName === 'CUSTOM_CHALLENGE') {
-    //         console.log ("CUSTOM_CHALLENGE");
-    //         // to send the answer of the custom challenge
-    //         Auth.sendCustomChallengeAnswer(user, password)
-    //             .then(user => console.log(user))
-    //             .catch(err => console.log(err));
-    //     } else if (user.challengeName === 'SHACKNEWS_PASSWORD') {
-    //       console.log ("SHACKNEWS_PASSWORD");
-    //       // to send the answer of the custom challenge
-    //       Auth.sendCustomChallengeAnswer(user, password)
-    //           .then(user => console.log(user))
-    //           .catch(err => console.log(err));
-    //   } else {
-    //         console.log(user);
-    //     }
-    //   })
-    //   .catch(err => console.log(err));
-
-
-    // let apiName = 'ShackmeetsApi';
-    // let path = '/users/login'; 
-    // let request = {
-    //   body: {
-    //     username: username,
-    //     password: password
-    //   }
-    // };
-
-    // const response = await API.post(apiName, path, request).then(response => {
-    //   console.log(response);
-
-    //   if (response.isValid) {
-    //     setUser({id: 1, username: 'omnova'})
-
-    //     return true;
-    //   }
-    //   else {
-    //     console.log('Invalid login');
-    //     return false
-    //   }
-    // }).catch(error => {
-    //   console.log(error.response)
-    //   return false;
-    // });
-    
+        console.log('error signing in', error);    }
   }, []);
 
   const logout = useCallback(async () => {
